1433b7cec035cf168ab27f4f80855a6bd20ad57e
kremen.val@gmail.com <kremen.val@gmail.com>
2020-06-03 23:21:09 +0300

    Added lesson 3

--- js/index.js
+++ js/index.js
@@ -1,198 +1,95 @@
-/* Start 2.1 */
+/* Start 3.1 */
 
-// x = getNumber('x');
-// y = getNumber('y');
+range = 10;
+massNumbers = new Array(range);
+massPrimes = [];
 
-// for (start = x; start <= y; start++) {
-// 	if (!(start % 2)) {
-// 		console.log('Sqrt of ' + start + ' is: ' + Math.pow(start, 2));
-// 	}
-// }
+for (index = min = max = 0, indexPrime = 0; index < range; index++) {
+	massNumbers[index] = getRandomInt(-50, 50);
 
-// function getNumber(nameVariable) {
-// 	variable = parseInt(prompt('Enter a number for varible \'' + nameVariable + '\':', 5), 10);
+	if (isPrime(massNumbers[index])) {
+		massPrimes[indexPrime++] = massNumbers[index];
+	}
 
-// 	if (nameVariable == 'y') {
-// 		while (isNaN(variable) || variable - x < 10) {
-// 			variable = parseInt(prompt('You entered not a number or number of variable \''
-// 			+ nameVariable + '\' should be larger than \'x\' ( x = ' + x + ') by 10, enter a number:', 5), 10);
-// 		}
-// 	} else {
-// 		while (isNaN(variable)) {
-// 			variable = parseInt(prompt('You entered not a number for varible \''
-// 			+ nameVariable + '\', enter a number:', 5), 10);
-// 		}
-// 	}
+	if (!index) {
+		min = max = massNumbers[index]
+	} else if (index && massNumbers[index] < min) {
+		min = massNumbers[index];
+	} else if (index && massNumbers[index] > max) {
+		max = massNumbers[index];
+	}
+}
 
-// 	return variable;
-// }
+console.log('Array of numbers is: ' + massNumbers);
+console.log('Array of primes is: ' + massPrimes);
+console.log('Min number is: ' + min);
+console.log('Max number is: ' + max);
 
-/* End 2.1 */
+/* End 3.1 */
 
-/* Start 2.2 */
+/* Start 3.2 */
 
-// resultPrime = isPrime();
+commonMass = new Array(range);
 
-// if (resultPrime[1]) {
-// 	console.log(resultPrime[0] + ' is a prime number!');
-// } else {
-// 	console.log(resultPrime[0] + ' isn\'t a prime number!');
-// }
+for (i = newMin = newMax = 0; i < range; i++) {
+	commonMass[i] = getRandomInt(min, max);
 
-// function isPrime() {
-// 	primeNumber = parseInt(prompt('Enter a prime number:', 5), 10);
+	if (!i) {
+		newMin = newMax = [commonMass[i], i]
+	} else if (i && commonMass[i] < newMin[0]) {
+		newMin = [commonMass[i], i];
+	} else if (i && commonMass[i] > newMax[0]) {
+		newMax = [commonMass[i], i];
+	}
+}
 
-// 	while (isNaN(primeNumber)) {
-// 		primeNumber = parseInt(prompt('You entered not a number, enter a number:', 5), 10);
-// 	}
+console.log('Array of numbers is: ' + commonMass);
+console.log('NewMin number is: ' + newMin[0]);
+console.log('NewMax number is: ' + newMax[0]);
 
-// 	if (primeNumber == 2) {
-// 		return [primeNumber, true];
-// 	} else if (primeNumber < 2 || primeNumber % 2 === 0) {
-// 		return [primeNumber, false];
-// 	}
+/* End 3.2 */
 
-// 	for (i = 3, sqrt = Math.sqrt(primeNumber); i <= sqrt; i += 2) {
-// 		if (primeNumber % i === 0) {
-// 			return [primeNumber, false];
-// 		}
-// 	}
+/* Start 3.3 */
 
-// 	return [primeNumber, true];
-// }
+commonMass[newMin[1]] += commonMass[newMax[1]];
+commonMass[newMax[1]] = commonMass[newMin[1]] - commonMass[newMax[1]];
+commonMass[newMin[1]] -= commonMass[newMax[1]];
 
-/* End 2.2 */
+console.log('Array of numbers is: ' + commonMass);
 
-/* Start 2.3 (Сделал 2-мя методами с разной скоростью работы) */ 
+/* End 3.3 */
 
-// console.time('Recurtion time');
-// console.log('Summ of primes due to recursion is: ' + primeRecursion(2, 3));
-// console.timeEnd('Recurtion time');
+/* Start 3.4 */
 
-// function primeRecursion(summ, number)
-// {
-// 	if (number < 2 || number % 2 === 0) {
-// 		return primeRecursion(summ, number += 1);
-// 	}
+for (start = 0, end = commonMass.length - 1, halfRange = Math.floor(range / 2); start < halfRange; start++, end--) {
+	commonMass[start] += commonMass[end];
+	commonMass[end] = commonMass[start] - commonMass[end];
+	commonMass[start] -= commonMass[end];
+}
 
-// 	for (i = 3, sqrt = Math.sqrt(number); i <= sqrt; i += 2) {
-// 		if (number % i === 0) {
-// 			return primeRecursion(summ, number += 1);
-// 		}
-// 	}
+console.log('Revers array is: ' + commonMass);
 
-// 	if (number >= 250) {
-// 		return summ;
-// 	} else {
-// 		return primeRecursion(summ += number, number += 1);
-// 	}
-// };
 
-// console.time('Cycle time');
+/* End 3.4 */
 
-// for (i = 2, summPrime = 0; i <= 250; i++) {
-// 	for (j = 2, prime = true; j < i; j++) {
-// 		if (i % j == 0) {
-// 			prime = false;
-// 			break;
-// 		}
-// 	}
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min;
+}
 
-// 	if (prime) {
-// 		summPrime += i;
-// 	}
-// }
+function isPrime(primeNumber) {
+	if (primeNumber == 2) {
+		return true;
+	} else if (primeNumber < 2 || primeNumber % 2 === 0) {
+		return false;
+	}
 
-// console.log('Summ of primes due to cycle is: ' + summPrime);
-// console.timeEnd('Cycle time');
+	for (i = 3, sqrt = Math.sqrt(primeNumber); i <= sqrt; i += 2) {
+		if (primeNumber % i === 0) {
+			return false;
+		}
+	}
 
-/* End 2.3 */
-
-/* Start 2.4 */
-
-// numberFibonacci = parseInt(prompt('Enter a number bigger than 0:', 5), 10);
-
-// while (isNaN(numberFibonacci) || numberFibonacci < 0) {
-// 	numberFibonacci = parseInt(prompt('You entered not a number or number less than 0, enter a number:', 5), 10);
-// }
-
-// console.log('Fibonacci number for number ' + numberFibonacci + ' is: ' + fibonacci(numberFibonacci));
-
-// function fibonacci(number) {
-// 	if (number == 0) {
-// 		return number;
-// 	}
-
-// 	firtsPart = secondPart = 1;
-
-// 	for (i = 3; i <= number; i++) {
-// 		temp = firtsPart + secondPart;
-// 		firtsPart = secondPart;
-// 		secondPart = temp;
-// 	}
-
-// 	return secondPart;
-// };
-
-/* End 2.4 */
-
-/* Start 2.5 */
-
-// a = getNumberTask5('a');
-// b = getNumberTask5('b');
-// h = getNumberTask5('h');
-
-// for (step = a, result = 0; step < b; step += h) {
-// 	result += factorial(step);
-// }
-
-// console.log('Summ of factorials: ' + result);
-
-// function factorial(number) {
-// 	for (index = 1, summFactorial = 1; index <= number; index++) {
-// 		summFactorial *= index;
-// 	}
-
-// 	return summFactorial;
-// }
-
-// function getNumberTask5(nameVariable) {
-// 	variable = parseInt(prompt('Enter a number for varible \'' + nameVariable + '\':', 5), 10);
-
-// 	if (nameVariable == 'b') {
-// 		while (isNaN(variable) || variable - a < 5) {
-// 			variable = parseInt(prompt('You entered not a number or number of variable \''
-// 			+ nameVariable + '\' should be larger than \'a\' (a = ' + a + ') by 5, enter a number:', 5), 10);
-// 		}
-// 	} else if (nameVariable == 'a') {
-// 		while (isNaN(variable)) {
-// 			variable = parseInt(prompt('You entered not a number for varible \''
-// 			+ nameVariable + '\', enter a number:', 5), 10);
-// 		}
-// 	} else {
-// 		while (isNaN(variable) || variable < 1) {
-// 			variable = parseInt(prompt('You entered not a number for varible \''
-// 			+ nameVariable + '\' or number less than 1, enter a number:', 5), 10);
-// 		}
-// 	}
-
-// 	return variable;
-// }
-
-/* End 2.5 */
-
-/* Start 2.6 */
-
-// document.write('<table><tr><td><strong>№</strong></td><td><strong>Even number</strong></td></tr>');
-// count = 1;
-
-// for (row = 1; row <= 100; row++) {
-// 	if (row % 2 == 0) {
-// 		document.write('<tr><td>' + count + '</td><td>' + row + '</td></tr>');
-// 		count++;
-// 	}
-// }
-
-// document.write('</table>');
-
-/* End 2.6 */
+	return true;
+}
