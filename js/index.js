0dc38ab838dc43d82b7310f4d777c85223d9c5d7
kremen.val@gmail.com <kremen.val@gmail.com>
2020-06-14 19:02:29 +0300

    added lesson 5

--- js/index.js
+++ js/index.js
@@ -1,99 +1,18 @@
-/* Start 4.1.1 */
+/* Start 5.1 */
 
-range = 3;
+firstMass = getIntegerArray();
+secondMass = getIntegerArray();
+thirdMass = getIntegerArray();
 
-mass = getIntegerArray(range);
-console.log('Array befor loop: ' + mass);
+console.log(firstMass, secondMass, thirdMass);
+console.log(getMaxs(firstMass, secondMass, thirdMass));
 
-for (i = 0, loopRange = (range % 2 ? range - 1 : range); i < loopRange; i += 2) {
-	temp = mass[i];
-	mass[i] = mass[i + 1];
-	mass[i + 1] = temp;
-}
-
-console.log('Array after loop: ' + mass);
-console.log('------------------');
-
-/* End 4.1.1 */
-
-/* Start 4.1.2 */
-
-range = 10;
-
-mass = getIntegerArray(range);
-average = Math.round(mass.reduce(function(firstNumber, secondNumber) {
-	return firstNumber + secondNumber;
-}) / range);
-
-console.log('Array: ' + mass);
-console.log('Average: ' + average);
-
-for (i = 0; i < range; i++) {
-	if (mass[i] > average) {
-		console.log(mass[i]);
+function getMaxs() {
+	for (var i = 0, result = ''; i < arguments.length; i++) {
+		result += maxArray(arguments[i]) + (i != arguments.length - 1 ? '; ' : '');
 	}
+	
+	return result;
 }
 
-console.log('------------------');
-
-/* End 4.1.2 */
-
-/* Start 4.1.3 */
-
-a = getNumberFromUser('A');
-b = getNumberFromUser('B');
-
-if (a > b) {
-	a += b;
-	b = a - b;
-	a -= b;
-}
-
-console.log(a, b);
-
-for (i = 0; i < mass.length; i++) {
-	if (mass[i] >= a && mass[i] <= b) {
-		console.log('Index: ' + i);
-	}
-}
-
-/* End 4.1.3 */
-
-/* Start 4.1.4 */
-
-console.log('Array befor loop: ' + mass);
-
-for (i = 0; i < mass.length - 1; i++) {
-	for (j = i + 1; j < mass.length; j++) {
-		if (mass[i] == mass[j]) {
-			mass.splice(j--, 1);
-		}
-	}
-}
-
-console.log('Array after loop: ' + mass);
-console.log('------------------');
-
-copyMass = getIntegerArray(range);
-console.log('Array befor loop: ' + copyMass);
-
-for (i = 0, tempMass = []; i < copyMass.length; i++) {
-	if (tempMass.indexOf(copyMass[i]) == -1) {
-		tempMass.push(copyMass[i]);
-	}
-}
-
-copyMass = tempMass;
-
-console.log('Array after loop: ' + copyMass);
-console.log('------------------');
-
-/* End 4.1.4 */
-
-/* Start 4.2 */
-
-mass = compareArray([1, 2, 3, 'hello', 4, 5], [1, 2, 3, true, 4, undefined, 6]);
-
-console.log(mass);
-
-/* End 4.2 */
+/* End 5.1 */
--- js/library.js
+++ js/library.js
@@ -1,13 +1,13 @@
 function getRandomInt(min, max) {
-	min = Math.ceil(min);
-	max = Math.floor(max);
+	var min = Math.ceil(min);
+	var max = Math.floor(max);
 	return Math.floor(Math.random() * (max - min)) + min;
 }
 
 function getIntegerArray(rangeArray = 10, min = -10, max = 10) {
-	mass = new Array(rangeArray);
+	var mass = new Array(rangeArray);
 
-	for (i = 0; i < rangeArray; i++) {
+	for (var i = 0; i < rangeArray; i++) {
 		mass[i] = getRandomInt(min, max);
 	}
 
@@ -15,7 +15,7 @@ function getIntegerArray(rangeArray = 10, min = -10, max = 10) {
 }
 
 function getNumberFromUser(nameVariable) {
-	variable = parseInt(prompt('Enter a number for the ' + nameVariable + ':', 5));
+	var variable = parseInt(prompt('Enter a number for the ' + nameVariable + ':', 5));
 
 	while (isNaN(variable)) {
 		variable = parseInt(prompt('You entered no number. Need to enter a number for ' + nameVariable, 5));
@@ -24,26 +24,22 @@ function getNumberFromUser(nameVariable) {
 	return variable;
 }
 
-function summIntegerElements(massElements) {
-	for (i = summ = 0; i < massElements.length; i++) {
-		if (Number.isInteger(massElements[i])) {
-			summ += massElements[i];
+function maxArray(mass = []) {
+	for (var i = max = 0; i < mass.length; i++) {
+		if (max <= mass[i]) {
+			max = mass[i];
 		}
 	}
 
-	return summ;
+	return max;
 }
 
-function compareArray(firstMass = [], secondMass = []) {
-	if (!firstMass.length) {
-		return secondMass;
-	} else if (!secondMass.length) {
-		return firstMass;
+function minArray(mass = []) {
+	for (var i = min = 0; i < mass.length; i++) {
+		if (min >= mass[i]) {
+			min = mass[i];
+		}
 	}
 
-	if (summIntegerElements(firstMass) >= summIntegerElements(secondMass)) {
-		return firstMass;
-	} else {
-		return secondMass;
-	}
+	return min;
 }
