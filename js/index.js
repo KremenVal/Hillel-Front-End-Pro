49408bc7cde4ae6027e459b56e7f326260a01892
kremen.val@gmail.com <kremen.val@gmail.com>
2020-06-18 17:08:46 +0300

    added task 6.1

--- js/index.js
+++ js/index.js
@@ -1,62 +1,17 @@
-/* Start 5.1 */
+/* Start 6.1 */
 
-var firstMass = getIntegerArray(),
-	secondMass = getIntegerArray(),
-	thirdMass = getIntegerArray();
+function f(a = 2, b = 3, c) {
+	return (typeof c === 'function' ? c(sum(a, b)) : sum(a, b));
 
-console.log(firstMass, secondMass, thirdMass);
-console.log(getMaxs(firstMass, secondMass, thirdMass));
-
-function getMaxs() {
-	for (var i = 0, result = ''; i < arguments.length; i++) {
-		result += maxArray(arguments[i]) + (i != arguments.length - 1 ? '; ' : '');
+	function sum(a, b) {
+		return a + b;
 	}
-	
-	return result;
 }
 
-/* End 5.1 */
+console.log(f(5, 5, 'kek'));
 
-/* Start 5.2 */
+console.log(f(5, 5, function(a) {
+	return Math.pow(a, 2);
+}));
 
-// console.log(doFunction(2, 3, pow));
-
-// function doFunction(firstNumber = 5, secondNumber = 5, func) {
-// 	return func(firstNumber, secondNumber);
-// }
-
-// function pow(num1, num2) {
-// 	return num1 ** num2;
-// }
-
-/* End 5.2 */
-
-/* Start 5.3 */
-
-// var mass = getIntegerArray(),
-// 	copyMass = copyArray(mass);
-
-// console.log(mass, copyMass);
-
-/* End 5.3 */
-
-/* Start 5.4 */
-
-// var mass = getIntegerArray(),
-// 	copyMass = copy(mass, function(num){
-// 		return num * 10;
-// 	});
-
-// function copy(mass = [], func) {
-// 	var returnArray = [];
-
-// 	for (var i = 0; i < mass.length; i++) {
-// 		returnArray.push(typeof func !== 'function' ? mass[i] : func(mass[i]));
-// 	}
-
-// 	return returnArray;
-// }
-
-// console.log(mass, copyMass);
-
-/* End 5.4 */
+/* End 6.1 */
