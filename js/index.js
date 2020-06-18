4fe671b2a0d034dd021c703edf4f7f0aa2732df5
kremen.val@gmail.com <kremen.val@gmail.com>
2020-06-18 18:53:07 +0300

    added task 6.2

--- js/index.js
+++ js/index.js
@@ -1,17 +1,33 @@
 /* Start 6.1 */
 
-function f(a = 2, b = 3, c) {
-	return (typeof c === 'function' ? c(sum(a, b)) : sum(a, b));
+// function f(a = 2, b = 3, c) {
+// 	return (typeof c === 'function' ? c(sum(a, b)) : sum(a, b));
 
-	function sum(a, b) {
-		return a + b;
-	}
-}
+// 	function sum(a, b) {
+// 		return a + b;
+// 	}
+// }
 
-console.log(f(5, 5, 'kek'));
+// console.log(f(5, 5, 'kek'));
 
-console.log(f(5, 5, function(a) {
-	return Math.pow(a, 2);
-}));
+// console.log(f(5, 5, function(a) {
+// 	return Math.pow(a, 2);
+// }));
 
 /* End 6.1 */
+
+/* Start 6.2 */
+
+match = "hello";
+
+array = ["1234hello", "hellohd;lfmg", "45yu89egrhellowegojih", "dfhghello", ",bnsdklb"];
+
+kek = array.map(function(item) {
+	return item.indexOf(match);
+})
+.filter(item => item > -1)
+.join(', ');
+
+console.log(kek);
+
+/* End 6.2 */
