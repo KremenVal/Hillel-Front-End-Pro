e84d812806dd65a580ae668289335c2792a8b26a
kremen.val@gmail.com <kremen.val@gmail.com>
2020-07-07 21:01:37 +0300

    add lesson-10

--- js/index.js
+++ js/index.js
@@ -1,76 +1,25 @@
-/* Start 9.1 */
+/* Start 10.1 */
 
-mathOper = {
-	x: 12,
-	y: 3,
-	sign: '/'
-};
+let lastLi = document.querySelectorAll('ul > li > ul > li:last-of-type');
 
-function SuperMath() {
-};
+for (let li of lastLi) {
+	setTimeout(function() {
+		li.classList.add('last');
+	}, 2000);
+}
 
-SuperMath.prototype.input = function(msg, obj) {		/* Asking for a new parametrs */
-	alert(msg);
-	obj.x = prompt('Enter X:', 5);
-	obj.y = prompt('Enter Y:', 5);
-	obj.sign = prompt('Enter Sign:', '+');
-};
+function setFirstItemClassName(level = 1) {
+	if (level < 1 || level > 3) {
+		return false;
+	} else {
+		let patern = 'document.querySelectorAll(\'.root\')[0]' + '.children[0]'.repeat(level) + '.classList.add(\'first-item\')';
 
-SuperMath.prototype.checkData = function(obj) {			/* Cheking for a valid parametrs */
-	let msg = 'flag';
-
-	while (msg != '') {
-		if (isNaN(obj.x = parseInt(obj.x))) {
-			msg = 'X must be a number! ';
-		} else if (isNaN(obj.y = parseInt(obj.y))) {
-			msg = 'Y must be a number! ';
-		} else if (['+', '-', '/', '%', '*'].indexOf(obj.sign) == -1) {
-			msg = 'You need wrtie correct math operation! ';
-		} else if ((obj.sign == '/' || obj.sign == '%') && !obj.y) {
-			msg = 'Cannot divide by zero!\n' + `${obj.x + obj.sign + obj.y}\n`;
-		} else {
-			msg = '';
-		}
-	
-		if (msg != '') {
-			this.input(msg + 'Write new data.', obj);
-		}
+		setTimeout(function() {
+			eval(patern);
+		}, 2000);
 	}
 }
 
-SuperMath.prototype.mathOperation = function(x, y, sign) {		/* Performing math operations */
-	if (sign == '+') {
-		return x + y;
-	} else if (sign == '-') {
-		return x - y;
-	} else if (sign == '/') {
-		return x / y;
-	} else if (sign == '%') {
-		return x % y;
-	} else {
-		return x * y;
-	}
-}
+setFirstItemClassName(3);
 
-SuperMath.prototype.check = function(obj) {
-	this.checkData(obj);
-	
-	answer = prompt('Are you sure you want to perform this math operation? Write yes if you want to continue or no to change data.\n' + obj.x + obj.sign + obj.y, 'Yes');
-
-	while(answer.toLowerCase() != 'yes' && answer.toLowerCase() != 'no') {
-		answer = prompt('Write yes if you want to continue or no to change data.\n' + obj.x + obj.sign + obj.y, 'Yes');
-	}
-	
-	if (answer.toLowerCase() == 'yes') {
-		console.log(this.mathOperation(obj.x, obj.y, obj.sign));
-	} else {
-		this.input('Write new data.', obj);
-		this.check(obj);
-	}
-};
-
-
-p = new SuperMath();
-p.check(mathOper);
-
-/* End 9.1 */
+/* End 10.1 */
