5620c07142341de62f58e6310a182c17dbd96b2a
Kremen <kremen.netpeak@gmail.com>
2020-07-08 11:38:23 +0300

    Add some comment

--- js/index.js
+++ js/index.js
@@ -8,32 +8,34 @@ for (let li of lastLi) {
 	}, 2000);
 }
 
-// function setFirstItemClassName(level = 1) {
-// 	if (level < 1 || level > 3) {
-// 		return false;
-// 	} else {
-// 		let patern = 'document.querySelector(\'.root\')' + '.children[0]'.repeat(level) + '.classList.add(\'first-item\')';
-//
-// 		setTimeout(function() {
-// 			eval(patern);
-// 		}, 2000);
-// 	}
-// }
+/* Сделал 2 варианта, т.к. немного не понял какой из них подходит) */
 
 function setFirstItemClassName(level = 1) {
 	if (level < 1 || level > 3) {
 		return false;
 	} else {
-		for (let i = 0, children = document.querySelector('.root').children.length; i < children; i++) {
-			let patern = 'document.querySelector(\'.root\')' + `.children[${i}]` + '.children[0]'.repeat(level - 1)
-							+ '.classList.add(\'first-item\')';
-			
-			setTimeout(function() {
-				eval(patern);
-			}, 2000);
-		}
+		let patern = 'document.querySelector(\'.root\')' + '.children[0]'.repeat(level) + '.classList.add(\'first-item\')';
+
+		setTimeout(function() {
+			eval(patern);
+		}, 2000);
 	}
 }
+
+// function setFirstItemClassName(level = 1) {
+// 	if (level < 1 || level > 3) {
+// 		return false;
+// 	} else {
+// 		for (let i = 0, children = document.querySelector('.root').children.length; i < children; i++) {
+// 			let patern = 'document.querySelector(\'.root\')' + `.children[${i}]` + '.children[0]'.repeat(level - 1)
+// 							+ '.classList.add(\'first-item\')';
+//
+// 			setTimeout(function() {
+// 				eval(patern);
+// 			}, 2000);
+// 		}
+// 	}
+// }
 
 setFirstItemClassName(2);
 
