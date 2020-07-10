6e17271d1f7742cd03a8fc07486f4d7e3c83cadd
kremen.val@gmail.com <kremen.val@gmail.com>
2020-07-11 00:10:25 +0300

    add lesson 11

--- js/library.js
+++ js/library.js
@@ -53,3 +53,15 @@ function copyArray(mass = [], func) {
 
 	return returnArray;
 }
+
+function isNumber(string) {
+	let numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+	
+	for (let i = 0, len = string.length; i < len; i++) {
+		if (numbers.indexOf(string[i]) == -1) {
+			return false;
+		}
+	}
+
+	return true;
+}
