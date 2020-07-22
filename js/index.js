f4260dd52f9d4ce10e8e3e7d701d6b049ae20c16
kremen.val@gmail.com <kremen.val@gmail.com>
2020-07-22 22:56:12 +0300

    added lesson 13

--- js/index.js
+++ js/index.js
@@ -1,84 +1,42 @@
-/* Start 11.1 */
+/* Start 13.1 */
 
-let box = document.querySelector('.box'),
-	bams = document.querySelector('.bams'),
-	monitorParams = document.documentElement,
-	horizontalMovement = 0,
-	vertiaclMovement = 0
-	step = getStep('step');
-
-
-window.onkeydown = function(event) {
-	if (!box.classList.contains('animation-box')) {
-		if (event.keyCode === 37) {							/* Moving left */
-			box.style.left = (horizontalMovement -= step + (box.getBoundingClientRect().left
-				- Math.floor(box.getBoundingClientRect().left))) + 'px';
-			
-			if (box.getBoundingClientRect().left < 1) {
-				box.style.left = (horizontalMovement -= box.getBoundingClientRect().left) + 'px';
-				
-				setEffects('left', 'horizontalMovement', '+');
-			}
-		} else if (event.keyCode === 38) {					/* Moving top */
-			box.style.top = (vertiaclMovement -= step + (box.getBoundingClientRect().top
-				- Math.floor(box.getBoundingClientRect().top))) + 'px';
-			
-			if (box.getBoundingClientRect().top < 1) {
-				box.style.top = (vertiaclMovement -= box.getBoundingClientRect().top) + 'px';
-
-				setEffects('top', 'vertiaclMovement', '+');
-			}
-		} else if (event.keyCode === 39) {					/* Moving right */
-			box.style.left = (horizontalMovement += step + (box.getBoundingClientRect().right
-				- Math.floor(box.getBoundingClientRect().right))) + 'px';
-
-			if (box.getBoundingClientRect().right > monitorParams.clientWidth - 1) {
-				box.style.left = (horizontalMovement += monitorParams.clientWidth
-					- box.getBoundingClientRect().right) + 'px';
-
-				setEffects('left', 'horizontalMovement', '-');
-			}
-		} else if (event.keyCode === 40) {					/* Moving bottom */
-			box.style.top = (vertiaclMovement += step) + 'px';
-
-			if (box.getBoundingClientRect().bottom > monitorParams.clientHeight - 1) {
-				box.style.top = (vertiaclMovement += monitorParams.clientHeight
-					- box.getBoundingClientRect().bottom - 0.5) + 'px';
-
-				setEffects('top', 'vertiaclMovement', '-');
-			}
-		}
-	}
-}
-
-function setEffects(position, movement, sign) {
-	setTimeout(function(){
-		bams.style.opacity = 1;
-		bams.classList.add('animation-text');
-		box.classList.add('animation-box');
-		
-		setTimeout(function() {
-			eval(`box.style.${position} = (${movement} ${sign}= ${step} * 2) + 'px'`);
-		}, 100);
-
-		setTimeout(function() {
-			bams.style.opacity = 0;
-			bams.classList.remove('animation-text');
-			box.classList.remove('animation-box');
-		}, 2000);
+let tr = Array.from(document.querySelectorAll('tr')),
+	arrayObjects = [],
+	summOfPrices = 0;
+	
+tr.reduce((mass, node) => {
+	let td =  Array.from(node.querySelectorAll('td'));
+	
+	mass.push({
+		name: td[0].textContent,
+		price: td[1].textContent
 	});
-}
 
-function getStep(nameVariable) {
-	let variable = prompt('Enter a number for the ' + nameVariable + ':', 5)
-		maxStep = monitorParams.clientHeight / 3;
+	return mass;
+}, arrayObjects);
 
-	while (!isNumber(variable) || parseInt(variable) < 1 || parseInt(variable) > maxStep) {
-		console.log(isNumber(variable));
-		variable = prompt(`You entered no number or number less than 1 or bigger than ${maxStep}. Need to enter a valid number for ` + nameVariable, 5);
-	}
+console.dir(arrayObjects);
 
-	return parseInt(variable);
-}
+/* End 13.1 */
 
-/* End 11.1 */
+/* Start 13.2 */
+
+arrayObjects.forEach((cinema) => {
+	if (cinema.name !== '' && cinema.price !== '') {
+		console.log(cinema);
+	};
+})
+
+/* End 13.2 */
+
+/* Start 13.3 */
+
+summOfPrices = arrayObjects.reduce((summ, cinema) => {
+	let price = parseInt(cinema.price);
+
+	return summ + (isNaN(price) ? 0 : price);
+}, 0);
+
+console.log(summOfPrices);
+
+/* End 13.3 */
