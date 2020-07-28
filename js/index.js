a5ac725f0db302f210b612b81cd5d3af4ae6b34f
kremen.val@gmail.com <kremen.val@gmail.com>
2020-07-28 18:49:33 +0300

    added task 15.2

--- js/index.js
+++ js/index.js
@@ -24,8 +24,42 @@ Array.prototype.__defineGetter__('render', function render() {
 	});
 });
 
-let arr = [1, 2, 3];
+let arr = [
+		1,
+		2,
+		3
+	];
 
-console.log(arr.render);
+// console.log(arr.render);
 
 /* End 15.1 */
+
+/* Start 15.2 */
+
+let data = {
+};
+
+Object.defineProperty(data, 'model', {
+	get: function() {
+		return this.symbol;
+	},
+	set: function(value) {
+		if (!this.myString) {
+			this.myString = value;
+		} else {
+			this.str = this.myString.split(value);
+			this.symbol = {
+				symbol: value,
+				count: this.myString.split(value).length - 1
+			}
+		}
+	}
+});
+
+data.model = 'Hello, how, are you?';
+data.model = '1';
+
+console.log(data);
+console.log(data.model);
+
+/* End 15.2 */
